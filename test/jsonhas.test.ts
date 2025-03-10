5dd435b6b36cd5b8c7e3a2b9df6e2f3edc29a1d8
xpwu <diodexp@foxmail.com>
2025-03-10 19:29:11 +0800

    Update jsonhas.test.ts

--- test/jsonhas.test.ts
+++ test/jsonhas.test.ts
@@ -1,4 +1,4 @@
-import {Json, JsonHas} from "../src/json";
+import {Json, JsonHas, JsonHasAll} from "../src/json";
 
 
 class User {
@@ -92,5 +92,27 @@ test("jsonhas-no-yes", ()=>{
   let has2 = JsonHas(r.user)
   expect(has2.name).toBeTruthy()
   expect(has2.age).toBeFalsy()
+})
+
+test("jsonhas-all", ()=>{
+  let user1 = new User()
+  expect(JsonHasAll(user1)).toBeFalsy()
+
+  const json1 = {
+    name: "xp1"
+  }
+  let [user2, err] = new Json().fromJson(json1, User)
+  expect(err).toBeNull()
+  expect(JsonHasAll(user2)).toBeFalsy()
+  expect(JsonHasAll(user2, "age")).toBeTruthy()
+  expect(JsonHasAll(user2, "age", "name")).toBeTruthy()
+
+  const json2 = {
+    name: "xp1",
+    age: 18
+  }
+  let [user3, err3] = new Json().fromJson(json2, User)
+  expect(err3).toBeNull()
+  expect(JsonHasAll(user3)).toBeTruthy()
 })
 
